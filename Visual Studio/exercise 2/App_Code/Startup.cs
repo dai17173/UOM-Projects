5428a3796dbea294424ee93caa383c573e05001a
Stavros Tsiaousis <72508643+dai17173@users.noreply.github.com>
2021-11-12 20:45:01 +0100

    Add files via upload

---
+++ Visual Studio/exercise 2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dai171733.Startup))]
+namespace dai171733
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
